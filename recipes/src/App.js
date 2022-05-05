770158eefe26538297a63a58ccf53c137859581e
rupert-denton <rupertdenton1@gmail.com>
2022-05-05 12:31:28 +1200

    created page container

---
+++ recipes/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg'
+import './App.css'
+
+import PageContainer from './components/ui/PageContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <PageContainer></PageContainer>
+    </div>
+  )
+}
+
+export default App
---
+++ recipes/src/components/ui/PageContainer.js
@@ -0,0 +1,7 @@
+import './PageContainer.css'
+
+function PageContainer() {
+  return <div className="page-container"></div>
+}
+
+export default PageContainer
