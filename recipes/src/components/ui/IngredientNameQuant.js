50ad232f4e1ef1e4a6a4d821fb4da1b252aece43
rupert-denton <rupertdenton1@gmail.com>
2022-05-09 15:06:14 +1200

    fixed merge errors

--- recipes/src/components/ui/IngredientNameQuant.js
+++ recipes/src/components/ui/IngredientNameQuant.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import './IngredientNameQuant.css'
-
-function IngredientNameQuant() {
-  return (
-    <div>
-      <div class="ingredient-quantity">1 tsp.</div>
-      <div class="ingredient-name">Black Peppercorns</div>
-    </div>
-  )
-}
-
-export default IngredientNameQuant
-||||||| b56038d
-=======
 import './IngredientNameQuant.css'
 
 function IngredientNameQuant() {
@@ -25,4 +10,3 @@ function IngredientNameQuant() {
 }
 
 export default IngredientNameQuant
->>>>>>> e28c84d4273692fcc75eaf17b4cf2b90d43ec026
--- recipes/src/index.js
+++ recipes/src/index.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-||||||| b56038d
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
@@ -26,19 +8,7 @@ import Recipe from './routes/Recipe'
 import RecipeForm from './routes/RecipeForm'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
->>>>>>> e28c84d4273692fcc75eaf17b4cf2b90d43ec026
 root.render(
-<<<<<<< HEAD
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-||||||| b56038d
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-=======
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
@@ -47,9 +17,7 @@ root.render(
     </Routes>
   </BrowserRouter>
 )
->>>>>>> e28c84d4273692fcc75eaf17b4cf2b90d43ec026
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
