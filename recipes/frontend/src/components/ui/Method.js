cb29fdd9ab1aa189ec57fddffda7f3917b671cf0
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-05-25 20:59:10 +1200

    finished recipe method add feature

--- recipes/frontend/src/components/ui/Method.js
+++ recipes/frontend/src/components/ui/Method.js
@@ -1,7 +1,9 @@
 import './Method.css'
 
 import MethodStep from './MethodStep'
+
 function Method(props) {
+  console.log(props)
   return (
     <div className="method">
       <MethodStep recipeMethod={props.recipeMethod}></MethodStep>
