a38616f11656602e93a7a116cb3ece99512419b2
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-06-01 09:08:33 +1200

    completed POSTing for recipes and GETtting for recipes

--- recipes/frontend/src/components/ui/MethodStep.js
+++ recipes/frontend/src/components/ui/MethodStep.js
@@ -7,13 +7,16 @@ function mapMethod() {
   return methodArray.map((item, idx) => (
     <React.Fragment key={idx}>
       <div className="method-step small-header">Step {idx + 1}</div>
-      <div className="method-text">{item.step_instruction}</div>
+      <div className="method-text">{item}</div>
     </React.Fragment>
   ))
 }
 
 function MethodStep(props) {
-  methodArray = props.recipeMethod || []
+  console.log(props)
+  methodArray = props.recipeMethod
+  // methodArray = array.length !== 0 ? array.split(/\r?\n/) : []
+
   return <div className="recipe-method-container">{mapMethod()}</div>
 }
 
