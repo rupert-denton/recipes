3ebde9418105732e1e15b02f0e1f7cab5a142b5d
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-06-02 10:38:44 +1200

    finished recipe page build

--- recipes/frontend/src/components/ui/RecipeList.jsx
+++ recipes/frontend/src/components/ui/RecipeList.jsx
@@ -1,23 +1,36 @@
 import './RecipeList.css'
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+let recipeArray
+function mapRecipes() {
+  return recipeArray.map((recipeName, idx) => (
+    <React.Fragment key={idx}>
+      <div className="recipe">
+        <Link to={'recipe/' + recipeName}>{recipeName}</Link>
+      </div>
+    </React.Fragment>
+  ))
+}
 
 export default function RecipeList() {
   const [recipeList, setRecipeList] = useState([])
   const retrieveAllRecipes = function () {
-    fetch(`/`, {
+    fetch(`http://localhost:3001/`, {
       method: 'GET',
       headers: { 'Content-type': 'application/json' },
     })
       .then((resp) => resp.json())
       .then((json) => {
-        console.log(json)
+        let recipes = []
+        for (const item of json) {
+          recipes.push(item.recipe_name)
+        }
+        setRecipeList(recipes)
       })
   }
 
   retrieveAllRecipes()
-  return (
-    <div className="recipe-list">
-      {/* iterate through returned data (recipeList) and put here */}
-    </div>
-  )
+  recipeArray = recipeList || []
+  return <div className="recipe-list">{mapRecipes()}</div>
 }
--- recipes/frontend/src/routes/Recipe.jsx
+++ recipes/frontend/src/routes/Recipe.jsx
@@ -1,5 +1,17 @@
-import PageContainer from '../components/ui/PageContainer'
+import RecipeTitle from '../components/ui/RecipeTitle'
+import RecipeIngredientsContainer from '../components/ui/RecipeIngredientsContainer'
+import RecipeMethodContainer from '../components/ui/RecipeMethodContainer'
+import React from 'react'
 
 export default function Recipe() {
-  return <PageContainer />
+  return (
+    <div>
+      <div className="recipe-form-container">
+        <form className="recipe-form">
+          Title
+          <div className="ingredient-method-frame"></div>
+        </form>
+      </div>
+    </div>
+  )
 }
