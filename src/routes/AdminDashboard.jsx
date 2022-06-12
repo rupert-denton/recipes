9214d00aa77072fe0906348e4bea3226b3587201
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-06-12 19:55:41 +1200

    started dashboard page

---
+++ src/routes/AdminDashboard.jsx
@@ -0,0 +1,8 @@
+//external dependencies
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+export default function AdminDashboard() {
+  ;<Link to="/">Front Page</Link>
+}
