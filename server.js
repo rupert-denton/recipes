97248eb809f5375357677206ef50030bd0f5e5e4
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-06-21 20:21:01 +1200

    updated path

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const bodyParser = require('body-parser')
 server.use(express.json())
 server.use(bodyParser.json())
 server.use('/api/recipes', require('./routes'))
-server.('/', function(req, res) {
-  res.sendFile( path.resolve('build/index.html') );
-});
+server.get('/', function (req, res) {
+  res.sendFile(path.resolve('build/index.html'))
+})
 
 module.exports = server
