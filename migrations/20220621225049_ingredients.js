e059fe9fddca4085af864b86f870f23c3c34fa44
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-06-22 11:02:26 +1200

    added migrations

---
+++ migrations/20220621225049_ingredients.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('ingredients', (table) => {
+    table.increments('id').primary() //id
+    table.string('ingredient_name')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('ingredients')
+}
