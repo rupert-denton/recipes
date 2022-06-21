8cda72d4a88e949aba53bf672e16599bb797d7e2
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-06-21 20:12:37 +1200

    added build path to server.js

--- server.js
+++ server.js
@@ -6,5 +6,8 @@ const bodyParser = require('body-parser')
 server.use(express.json())
 server.use(bodyParser.json())
 server.use('/api/recipes', require('./routes'))
+server.get(“*”, (req, res) => {
+  res.sendFile('./build/index.html')
+})
 
 module.exports = server
