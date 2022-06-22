2cd201c13f57f31b4117fb08695199ebeda09c48
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-06-23 07:27:05 +1200

    Fixed css for front page

--- src/components/ui/RecipeList.jsx
+++ src/components/ui/RecipeList.jsx
@@ -3,28 +3,39 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function RecipeList() {
-  // use state as a source of truth for the array of recipes
   const [recipes, setRecipes] = useState([])
-
-  // use effect will fetch the data (BUT not every render,
-  // only once when the component FIRST renders)
-  // in simple terms: the empty array at the end tells useEffect to only run once
   useEffect(() => {
     fetch('/api/recipes')
       .then((res) => res.json())
-      // set the state to the array of recipes, you could use .map here to only get recipe names
-      // setRecipes(data.map(recipe => recipe.recipe_name))
+
       .then((data) => {
         setRecipes(data)
       })
       .catch((err) => console.log(err))
   }, [])
 
-  // pull it out into a variable like this or into another component
   const recipeList = recipes.map((recipe, idx) => (
     <React.Fragment key={idx}>
-      <div className="recipe">
-        <Link to={'recipe/' + recipe.id}>{recipe.recipe_name}</Link>
+      <div className="recipe-card">
+        <div className="recipe-image">
+          <img
+            alt="placeholder for a recipe"
+            className="recipe-image"
+            src={'/place-holder.png'}
+          />
+        </div>
+        <div className="recipe">
+          <div className="recipe-card-header">
+            <Link className="recipe-link" to={`recipe/${recipe.id}`}>
+              {recipe.recipe_name}
+            </Link>
+          </div>
+          <div className="recipe-slug">
+            <p className="slug-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+            </p>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   ))
@@ -32,7 +43,7 @@ export default function RecipeList() {
   return (
     <div className="recipe-list">
       <h1>Recipes</h1>
-      {recipeList}
+      <div className="recipes-list-container">{recipeList}</div>
     </div>
   )
 }
